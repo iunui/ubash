4e50d5dd3fff88e3dd860fee3aa3311c51296fcc
iunui <30671755+iunui@users.noreply.github.com>
2018-01-15 14:47:50 +0700

    Update ip240.sh

--- ip240.sh
+++ ip240.sh
@@ -4,5 +4,5 @@ sudo wget https://raw.githubusercontent.com/iunui/ubash/master/interfaces
 sudo mv /etc/network/interfaces /etc/network/interfaces.auto
 sudo cp ./interfaces /etc/network/interfaces
 
-# restart networking
-sudo service networking restart
+# restart networking eth0, enp0s2
+sudo ifdown enp0s2 && sudo ifup enp0s2
